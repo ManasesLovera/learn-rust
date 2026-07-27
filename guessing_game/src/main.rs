fa1d1e37680cbc2d627e09bd05a79f85b7840350
Manases Lovera <manaseslovera07@gmail.com>
2026-07-27 17:56:44 -0400

    showing if guessed number is greater, smaller or equal to secret number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,9 @@
 use std::io;
+use rand::random_range;
+use std::cmp::Ordering;
 
-fn main() {
-    println!("Guess the number!");
-
-    let secret_number: i32 = rand::random_range(1..=10);
+fn main() { println!("Guess the number!");
+    let secret_number: i32 = random_range(1..=5);
 
     println!("Please input your guess.");
 
@@ -13,7 +13,23 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess_number: i32 = match guess.trim_end().parse::<i32>() {
+
+        Ok(num) => num,
+        Err(_) => {
+            println!("Error parsing guess number, using 0 as default");
+            0
+        }
+    };
+
+
     println!("You guessed: {guess}");
+
+    match guess_number.cmp(&secret_number) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("You win"),
+    }
 
     println!("The correct secret number: {secret_number}");
 }

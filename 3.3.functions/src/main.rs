b1b2935544171222ba8342dfb938331aa789fc05
Manases Lovera <manaseslovera07@gmail.com>
2026-07-28 02:05:29 -0400

    chore: finish functions in rust

--- 3.3.functions/src/main.rs
+++ 3.3.functions/src/main.rs
@@ -1,11 +1,25 @@
-
-fn another_function() {
-    println!("Another function");
-}
-
 fn main() {
 
     println!("Hello, world!");
 
-    another_function();
+    another_function(45);
+    print_labeled_measurement(5, 'h');
+
+    let x = five();
+
+    println!("The value is {x}");
 }
+
+fn another_function(x: i32) {
+    println!("The value of x is {x}");
+}
+
+fn five() -> u8 {
+    5
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+
+    println!("The measurement is: {value}{unit_label}");
+}
+

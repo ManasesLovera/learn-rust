50d89f34514fc460676a28c3462de887b1571d44
Manases Lovera <manaseslovera07@gmail.com>
2026-07-30 18:35:08 -0400

    chore: starting understanding ownership

---
+++ 4.ownership/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    
+    let s = "hello";
+
+    {
+        let s = "hello2";
+        println!("{s}");
+    }
+
+    println!("{s}");
+
+    // ---
+    
+    let _s = String::from("hello");
+
+    let mut s = String::from("hello");
+    s.push_str(", world");
+
+    println!("{s}");
+}

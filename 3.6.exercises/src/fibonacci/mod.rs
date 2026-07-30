2eab5a02bf8c4aea2d96c7f889f0d68959b2d72e
Manases Lovera <manaseslovera07@gmail.com>
2026-07-30 16:28:06 -0400

    feat: fibonacci exercise completed

--- 3.6.exercises/src/fibonacci/mod.rs
+++ 3.6.exercises/src/fibonacci/mod.rs
@@ -1,4 +1,16 @@
-pub fn get_nth(n: i32) -> u64 {
-    todo!()
+
+
+pub fn get_nth(n: usize) -> u64 {
+    
+    if n == 0 { return 0; }
+    if n == 1 { return 1; }
+
+    let mut state = (0, 1); // Represents (current, next)
+    
+    for _ in 2..=n {
+        state = (state.1, state.0 + state.1);
+    }
+
+    state.1
 }
 
--- 3.6.exercises/src/main.rs
+++ 3.6.exercises/src/main.rs
@@ -2,6 +2,7 @@ mod fibonacci;
 
 fn main() {
 
+    println!("Fibonacci for 23:");
     let result = fibonacci::get_nth(23);
     println!("Result: {result}");
 }

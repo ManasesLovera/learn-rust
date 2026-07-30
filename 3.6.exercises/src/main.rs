67e2e7c10182b2a38c5a2ff045d573cc338de2b4
Manases Lovera <manaseslovera07@gmail.com>
2026-07-30 16:33:22 -0400

    feat: fahrenheit to celsius completed in rust

--- 3.6.exercises/src/main.rs
+++ 3.6.exercises/src/main.rs
@@ -1,8 +1,14 @@
 mod fibonacci;
+mod temp_converter;
 
 fn main() {
 
     println!("Fibonacci for 23:");
     let result = fibonacci::get_nth(23);
+    println!("Result: {result}");
+
+    println!("Convert from Fahrenheit to Celsius: 98.6");
+    let result = temp_converter::fahrenheit_to_celsius(98.6);
+
     println!("Result: {result}");
 }
---
+++ 3.6.exercises/src/temp_converter/mod.rs
@@ -0,0 +1,5 @@
+
+pub fn fahrenheit_to_celsius(f: f64) -> f64 {
+    (f - 32.0) * 5.0 / 9.0
+}
+

0f1c941d8070796cdb5de7a0bd8c1ecd7a6eb403
Manases Lovera <manaseslovera07@gmail.com>
2026-07-31 23:04:56 -0400

    feat: slice types in rust

---
+++ 4.3.slice-type/src/main.rs
@@ -0,0 +1,19 @@
+/*
+Write a function that takes a string of words separated by spaces and returns the first word it finds in that string. If the function doesn’t find a space in the string, the whole string must be one word, so the entire string should be returned.
+*/
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
+
+fn main() {
+    println!("Hello, world!");
+}

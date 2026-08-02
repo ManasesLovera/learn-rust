9c39463be48992ce6739dc22190bcebce745f6d5
Manases Lovera <manaseslovera07@gmail.com>
2026-08-02 19:40:59 -0400

    chore: struct formatting

--- 5.2.struct-example/src/main.rs
+++ 5.2.struct-example/src/main.rs
@@ -1,5 +1,6 @@
 // An Example Program Using Structs
 
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -9,6 +10,28 @@ fn main() {
         width: 30,
         height: 50,
     };
+
+    println!("rect1 is {rect1:?}");
+    println!("rect1 is {rect1:#?}");
+    /* Output:
+
+    rect1 is Rectangle { width: 30, height: 50 }
+    rect1 is Rectangle {
+        width: 30,
+        height: 50,
+    }
+
+    */
+
+    dbg!(&rect1);
+    /* Output:
+
+    [src/main.rs:26:5] &rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    }
+
+    */
 
     println!(
         "The area of the rectangle is {} square pixels.",

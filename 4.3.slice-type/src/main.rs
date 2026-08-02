485ace1e412c849f5cde86265a3575568de2162f
Manases Lovera <manaseslovera07@gmail.com>
2026-08-02 19:09:10 -0400

    chore: practicing more about ownership, borrowing, slice

--- 4.3.slice-type/src/main.rs
+++ 4.3.slice-type/src/main.rs
@@ -2,27 +2,30 @@
 Write a function that takes a string of words separated by spaces and returns the first word it finds in that string. If the function doesn’t find a space in the string, the whole string must be one word, so the entire string should be returned.
 */
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &String) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[0..i];
         }
     }
 
-    s.len()
+    &s[..]
 }
 
 fn main() {
     let mut s = String::from("hello world");
 
-    let word_count: usize = first_word(&s);
+    let first_word = first_word(&s);
 
-    println!("Value of word: {word_count}");
-
-    let word = &s[0..word_count];
-    println!("The total word is: {word}");
+    println!("The first word is: '{first_word}'");
 
     s.clear();
+
+    let a = [1, 2, 3, 4, 5];
+
+    let slice = &a[1..3];
+
+    assert_eq!(slice, &[2, 3]);
 }

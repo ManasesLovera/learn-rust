aeaec462787234c4edeec6ddadef7be51fa91ca9
Manases Lovera <manaseslovera07@gmail.com>
2026-08-02 19:28:32 -0400

    feat: create fn to create user template

--- 5.0.structs/src/main.rs
+++ 5.0.structs/src/main.rs
@@ -5,6 +5,16 @@ struct User {
     sign_in_count: u64,
 }
 
+fn build_user(email: String, username: String) -> User {
+    // Using field init shorthand
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
+
 fn main() {
     let mut user1 = User {
         active: true,
@@ -18,4 +28,11 @@ fn main() {
     user1.email = String::from("anotheremail@example.com");
 
     println!("user1.email = {0}", user1.email);
+
+    let mut user2 = build_user(
+        String::from("myemail@example.com"),
+        String::from("myusername"),
+    );
+
+    println!("user2 email: {0}", user2.email);
 }

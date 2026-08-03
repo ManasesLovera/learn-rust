66da1ab6172d5e283d48ea4581d19a6a426271dc
Manases Lovera <manaseslovera07@gmail.com>
2026-08-02 20:54:33 -0400

    feat: example with Option

--- 6.2.match-control-flow/src/main.rs
+++ 6.2.match-control-flow/src/main.rs
@@ -7,14 +7,23 @@ enum Coin {
 
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
-        Coin::Penny => {
-            println!("Lucky penny!");
-            1
-        }
+        Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
         Coin::Quarter => 25,
     }
 }
 
-fn main() {}
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn main() {
+    let coin = Coin::Penny;
+    let value_in_cent = value_in_cents(coin);
+
+    println!("The value in cents is: {value_in_cent}");
+}
